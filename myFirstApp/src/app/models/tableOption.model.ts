838083ad5ca74bc30d3075f801af655e437ad9b0
Chris <hdin898@aucklanduni.ac.nz>
2019-04-26 20:11:55 +1200

    half working dropdown list

---
+++ myFirstApp/src/app/models/tableOption.model.ts
@@ -0,0 +1,3 @@
+export class tableOption {
+    name: string;
+}
--- myFirstApp/src/app/page1/page1.component.ts
+++ myFirstApp/src/app/page1/page1.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { debug } from 'util';
 import { FormControl } from '@angular/forms';
+import { tableOption } from '../models/tableOption.model';
 
 @Component({
   selector: 'app-page1',
@@ -16,6 +17,14 @@ export class Page1Component implements OnInit {
   DesiredTable = new FormControl('');
   DesiredTableField = new FormControl('');
 
+  tableOptions: tableOption[] = [
+    {name: 'Members'},
+    {name: 'Products'},
+    {name: 'PriceBreaks'},
+    {name: 'Orders'},
+    {name: 'OrderDetails'}
+  ]
+
   constructor() { }
 
   ngOnInit() {
@@ -26,8 +35,16 @@ export class Page1Component implements OnInit {
   }
 
   findDesiredFromTable(): string {
+
     return ',' + this.NewVariable.value + ' = SELECT' + this.DesiredTableField.value + ' FROM ' + this.Account.value + '_'
             + this.FromTable.value + ' LEFT JOIN ' + this.Account.value + '_' + this.DesiredTable.value + ' ON FROMTABLE_1 '
             + ' = ' + 'idTABLE2' + ' WHERE ' + 'id' + this.Account.value + '_' + this.DesiredTable.value + ' = ' + 'FROMTABLE_1';
   }
+
+  defaultOption($scope) {
+    $scope.prop = {
+      defaultTable: 'Members'
+    };
+  }
+  
 }
